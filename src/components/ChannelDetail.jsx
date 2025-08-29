956cf6418a5fa762f2146f1b25eef9bf5e0566a7
acgovardhan <mail2acgovardhan@gmail.com>
2025-08-30 00:05:22 +0530

    video feature update

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { Box } from '@mui/material'
+
+import { Videos, ChannelCard } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const ChannelDetail = () => {
+  const { id } = useParams();
+  const [channelDetail, setChannelDetail] = useState(null)
+  const [videos, setVideos] = useState([])
+
+  useEffect(()=>{
+    fetchFromAPI(`channels?part=snippet&id=${id}`)
+    .then((data)=> setChannelDetail(data?.items[0]));
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet%2Cid&order=date`)
+    .then((data)=> setVideos(data?.items));
+  },[id])
+
   return (
-    <div>
-      Channel
-    </div>
+    <Box minHeight="95vh">
+      <Box>
+        <div style={{background: 'radial-gradient(circle, rgba(63, 94, 251, 1) 0%, rgba(252, 70, 107, 1) 100%)',
+          zIndex: 10,
+          height: '300px',
+        }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop='-110px'/>
+      </Box>
+      <Box display="flex" p="2">
+        <Box sx={{ mr: { sm: '100px' }}}/>
+        <Videos videos={videos}/>
+        <div>hi</div>
+      </Box>
+    </Box>
   )
 }
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,7 +2,7 @@ import { Stack } from "@mui/material"
 import { categories } from "../utils/constants"
 import { Category } from "@mui/icons-material"
 
-const SideBar = () =>(
+const SideBar = ({ selectedCategory,setSelectedCategory }) =>(
     <Stack
       direction="row"
       sx={{
@@ -12,9 +12,17 @@ const SideBar = () =>(
       }}
     >
       {categories.map((category)=>(
-        <button>
-          <span>{category.icon}</span>
-          <span>{category.name}</span>
+        <button 
+          className="category-btn"
+          onClick={()=>setSelectedCategory(category.name)}
+          style={{
+            background: category.name === selectedCategory && '#FC1503',
+            color: 'white'
+          }}
+          key={category.name}
+          >
+          <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight:'15px'}}>{category.icon}</span>
+          <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8'}}>{category.name}</span>
         </button>
       ))}
 
